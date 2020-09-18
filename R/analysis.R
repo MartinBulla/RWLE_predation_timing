4fca4c4218fa7d1e72f1e46e83682717556fdc4e
MartinBulla <bulla.mar@gmail.com>
2020-09-18 12:34:41 +0200

    first comit

---
+++ R/analysis.R
@@ -0,0 +1,59 @@
+# LOAD TOOLS and DATA
+  require(here)
+  source(here::here('R/tools.R'))
+  
+  reml = TRUE # models fitted with REML (TRUE) or ML (FALSE)
+  nsim <- 5000 # number of simulations for predictions
+  
+  source(here::here('R/prepare_data.R'))
+
+# 24h pattern
+  # ggplot histogram 
+    gg = ggplot(xx, aes(x = time_corr, fill = night)) + geom_histogram(binwidth = 1, center = 0.5) + 
+      scale_x_continuous(expand = c(0, 0), lim = c(0,24), breaks = seq(0,24, by = 1), labels = c(0,"","2","","4","","6","","8","","10","","12","","14","","16","","18","","20","","22","","24"), name = "Time of day [hour]") + scale_y_continuous(expand = c(0, 0), name ="Predation events [count]")+
+      scale_fill_manual(values=c(day = 'lightgrey', night = 'darkgrey')) +
+      theme_MB +
+      theme(legend.title = element_blank())
+    
+    ggsave(file = 'Output/histogram.png', gg, dpi = 300, width = 7, height = 5, units = 'cm')
+
+    #hist(xx$time_corr,breaks=12)
+    #hist(xx$time_corr,breaks=24,xlim=c(0,24))
+    #summary(as.factor(xx$night))# check how many depredations in day/night
+    #summary(xx$sunset_num-xx$sunrise_num)# check the distribution of night lengths
+  # BASE  barplot
+    xx$time_corr_round=floor(xx$time_corr)
+    x1=ddply(xx,"time_corr_round",summarize,n=length(time_corr))
+    x1n=ddply(xx[xx$night=="night",],"time_corr_round",summarize,n=length(time_corr))
+    x2=data.frame(hr=c(0:23),n=0)
+    x2$n=x1$n[match(x2$hr,x1$time_corr_round)]
+    x2$n_night=x1n$n[match(x2$hr,x1n$time_corr_round)]
+    x2$n[is.na(x2$n)]=0
+    x2$n_night[is.na(x2$n_night)]=0
+
+    barplot(x2$n)# overall
+    barplot(t(cbind(x2$n_night,x2$n-x2$n_night)),beside = F,
+               col = c("grey30","grey70"))# with day/night
+
+# Seasonal pattern
+  # raw
+    plot(xx$time_corr~xx$date_num,cex=2,col=c("grey70","grey30")[xx$night],
+         pch=20,xlab="day in year",ylab= "hour of the day")
+    lines(x=seq(min(xx$date_num),max(xx$date_num)),y=sunss)
+    lines(x=seq(min(xx$date_num),max(xx$date_num)),y=sunrs)
+
+  # model
+    m=summary(glm(night~date_num,data=xx,family="binomial"))
+
+    plot(exp(m$coefficients[1,1] + seq(min(xx$date_num),max(xx$date_num)) * m$coefficients[2,1])/
+           (1+exp(m$coefficients[1,1] + seq(min(xx$date_num),max(xx$date_num)) * m$coefficients[2,1])) 
+         ~ seq(min(xx$date_num),max(xx$date_num)),
+         type="l",
+         ylab="probability of night vs day predation",
+         xlab="day in year",
+         ylim=c(0,1),
+         bty="n")
+    
+    points((rnorm(nrow(xx),0,.01)+(as.numeric(xx$night)-1))~xx$date_num,
+           col="grey30")# zatim ten hnusnej jitter, pak bych s tim kdyztak neco udelal...
+
---
+++ R/prepare_data.R
@@ -0,0 +1,38 @@
+x=fread("Data/pred.csv")
+    # column definitions
+      # nest - unique nest ID
+      # tnt:egg_temp - logger dostupnej pro dany hnizdo
+      # date - date (day.month.year) of predation event
+      # predation - hour (numeric) of predation event
+      # quality - 1 = good, 2 = partial predation events, after which the nest was abandoned, 3 = poor (CO TO ZNAMENA); for the analyses 1 and 2 were used 
+      # ..._time_correction - CO PRESNE TO ZNAMENA reseni nesrovnalosti s posunem casu - nutno k "predation" pricist maximum z nich
+
+xx = x[grep("ok",x$remark)] # use only good data
+
+# correct predation time where RFID was runninng on a wrong time 
+    xx$time_corr=xx$predation+xx$rfid_time_correction
+    xx$time_corr[xx$time_corr>24]=xx$time_corr[xx$time_corr>24]-24 # NEMELO BY SE TADY ZMENIT I DATUM?
+
+# add sunset and sunrise
+    koord=SpatialPoints(cbind(55.36449,24.83803), proj4string=CRS("+proj=longlat +datum=WGS84")) # center of the study area
+
+    xx$date=as.POSIXct(xx$date,format="%d.%m.%Y")
+    xx$date_num=as.numeric(strftime(xx$date,format="%j"))
+    xx$sunrise=crepuscule(koord,xx$date,direction="dawn",solarDep=6, POSIXct.out=TRUE)[,2]+4*3600
+    xx$sunset=crepuscule(koord,xx$date,direction="dusk",solarDep=6, POSIXct.out=TRUE)[,2]+4*3600
+    xx$sunrise_num=(as.numeric(xx$sunrise) %% (24*3600))/3600 
+    xx$sunset_num=(as.numeric(xx$sunset) %% (24*3600))/3600 
+
+    # dataset with sunsets and sunrises for all dates in a season
+        xx$date[xx$date_num==min(xx$date_num)]
+        xx$date[xx$date_num==max(xx$date_num)]
+        max(xx$date)
+        dats=seq.POSIXt(from=as.POSIXct("2019-03-08 UTC"),
+                        to=as.POSIXct("2019-07-14 UTC"),by=24*3600)
+        sunrs=crepuscule(koord,dats,direction="dawn",solarDep=6, POSIXct.out=TRUE)[,2]+4*3600
+        sunss=crepuscule(koord,dats,direction="dusk",solarDep=6, POSIXct.out=TRUE)[,2]+4*3600
+        sunrs=(as.numeric(sunrs) %% (24*3600))/3600 
+        sunss=(as.numeric(sunss) %% (24*3600))/3600 
+
+# add whether nest was depredated during day/night   
+    xx$night=as.factor(ifelse(xx$time_corr>= xx$sunrise_num & xx$time_corr <=xx$sunset_num,"day","night" ))

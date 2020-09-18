5f05fdd2bf23bc829b96b05e78d854e28355c8a8
MartinBulla <bulla.mar@gmail.com>
2020-09-18 16:02:27 +0200

    them_MB line width adjust

--- R/tools.R
+++ R/tools.R
@@ -21,14 +21,14 @@ using<-function(...) {
 
 # Customized ggplot theme
     theme_MB = theme(  
-              axis.line=element_blank(),
+              axis.line = element_blank(),
               #axis.line = element_line(colour="grey70", size=0.25),
               axis.title = element_text(size=7, colour="grey30"),
               axis.title.y = element_text(vjust=3.5),
               axis.title.x = element_text(vjust=1),
-              axis.text=element_text(size=6),#, vjust = 0.5, hjust=1),# margin=units(0.5,"mm")),
+              axis.text = element_text(size=6),#, vjust = 0.5, hjust=1),# margin=units(0.5,"mm")),
               axis.ticks.length=unit(0.5,"mm"),
-              axis.ticks = element_line(colour = "grey70"),
+              axis.ticks = element_line(colour = "grey70", size = 0.1),
               #axis.ticks.margin,
               
               strip.text.x = element_text(size = 6, color="grey30",  margin=margin(1,1,1,1,"mm")), #grey50
@@ -37,11 +37,17 @@ using<-function(...) {
                 #strip.text = element_blank(),
               panel.spacing = unit(0, "mm"),
               panel.background=element_blank(),
-                #panel.border=element_blank(),
-              panel.border = element_rect(colour="grey70", size=0.25, fill = NA),
+              panel.border = element_rect(colour="grey70", size=0.1, fill = NA), #panel.border=element_blank(),
               panel.grid = element_blank(),
+
               legend.text=element_text(size=6),
-              legend.title=element_text(size=7)
+              legend.title=element_text(size=6),
+              legend.key = element_rect(colour = NA, fill = NA),
+              legend.key.height= unit(0.5,"line"),
+              legend.key.width = unit(0.25, "cm"),
+              legend.margin = margin(0,0,0,0, unit="cm"),
+              legend.box.margin = margin(l = -6), #legend.justification = c(-1,0),
+              legend.background = element_blank()
               )
 
 
